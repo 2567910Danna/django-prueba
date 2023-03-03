1c0b034708932c62d47f2f12259d259fae21e8d7
sofia <sofiavanesapechene@gmail.com>
2023-03-03 13:39:02 -0500

    primer commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hola, me encuentro en la aplicacion home.")
+def index(request):
+    return HttpResponse("Hola, me encuentro en la aplicacion home.")
